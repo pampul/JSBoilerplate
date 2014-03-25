09f4fb3e7fdb264b27b7635fbf86fbd203a12cbd
Florian Mithieux <florian.mithieux@gmail.com>
2014-03-25 15:22:12 +0100

    Init project

---
+++ README.md
@@ -0,0 +1,68 @@
+# JSBoilerplate [![Build Status](https://travis-ci.org/Argetloum/AngularBoilerplate.png)](https://travis-ci.org/Argetloum/AngularBoilerplate)
+
+Quick Front-End Boilerplate using jQuery and ES6 ready thanks traceur-compiler, Sass and Gulp.
+
+JavaScript task running, build processes, autominification and file concatenation, wrapped with an enhanced HTML5 boilerplated.
+
+* Source: [github.com/Argetloum/AngularJSBoilerplate](http://github.com/Argetloum/AngularJSBoilerplate)
+
+
+## Jump start
+
+Get started with the boilerplate:
+
+1. Clone the git repo — `git clone https://github.com/Argetloum/AngularJSBoilerplate.git` and checkout the tagged release you need.
+2. Execute in the shell `sudo npm install` to install each node.js dependencies, needed for [Gulp.](http://gulpjs.com/)
+3. Execute in the shell `bower install` to install each front-end dependencies.
+4. Execute in the shell `gulp` to generate the stylesheets and scripts files in the `app/assets/` folder.
+
+## Features
+
+Here are some of the main features of the boilerplate:
+
+* jQuery lib
+* ES6 ready thanks to traceur-compiler
+* HTML5 semantics
+* Baseline HTML5 features, DNS prefetching, responsive meta
+* Encourages one-file CSS/JS in the view (HTML) for performance
+* Includes Modernizr and HTML5 Shiv
+* Google Universal Analytics snippet
+* Open source workflow with Gulp running on Node.js
+* Pre-setup Sass/SCSS files and folders for baseline project structure and imports
+* Standard .gitignore to ignore minified files and standard ignorables such as .DS_Store
+* JSHint .jshintrc file for configuring JavaScript linting
+
+## Scaffolding
+
+````
+├── app
+│   ├── assets
+│   │   ├── css
+│   │   ├── fonts
+│   │   ├── img
+│   │   └── js
+│   ├── layouts
+│   │   ├── header.html
+│   │   └── footer.html
+│   ├── partials
+│   │   └── home.html
+│   ├── apple-touch-icon-precomposed.png
+│   ├── favicon.ico
+│   └── index.html
+├── src
+│   ├── js
+│   │   └── app.js
+│   └── scss
+│       ├── mixins
+│       ├── modules
+│       ├── partials
+│       ├── vendor
+│       └── style.scss
+├── package.json
+├── README.md
+├── bower.json
+├── gulpfile.js
+├── .gitignore
+├── .bowerrc
+├── .jshintrc
+````
---
+++ gulpfile.js
@@ -0,0 +1,149 @@
+var gulp = require('gulp');
+
+var concat = require('gulp-concat');
+var uglify = require('gulp-uglify');
+var imagemin = require('gulp-imagemin');
+var jshint = require('gulp-jshint');
+var sass = require('gulp-sass');
+var bowerFiles = require('gulp-bower-files');
+var minifyCSS = require('gulp-minify-css');
+var rename = require('gulp-rename');
+var watch = require('gulp-watch');
+var notify = require('gulp-notify');
+var concatCSS = require('gulp-concat-css');
+
+var paths = {
+  bowerCopy: {
+    dest: 'app/assets/components'
+  },
+
+  scripts: {
+    src: [
+      'src/components/jquery/dist/jquery.js',
+      'src/components/bootstrap-sass/vendor/assets/javascripts/bootstrap.js',
+      'src/js/app.js'
+    ],
+    fullDir: 'app/assets/js/scripts.js',
+    dir: 'app/assets/js',
+    dest: 'scripts.js',
+    jsHint: [
+      'src/js/app.js',
+      'src/js/**/*.js'
+    ]
+  },
+  images: {
+    src: 'app/assets/img/**/*',
+    dest: 'app/assets/img'
+  },
+
+  scss: {
+    src: [
+      'src/scss/style.scss',
+      'src/components/bootstrap-sass/vendor/assets/stylesheets/bootstrap.scss'
+    ],
+    dest: 'app/assets/components/css',
+    finalDest: 'app/assets/css'
+  },
+
+  watch: {
+    styles: [
+      'src/scss/style.scss',
+      'src/scss/**/*.scss'
+    ],
+    js: [
+      'src/js/app.js',
+      'src/js/**/*.js'
+    ]
+  }
+};
+
+
+/**
+ * Stylesheets tasks
+ */
+gulp.task('styles', function () {
+  gulp.src(paths.watch.styles)
+    .pipe(watch(function (files) {
+      gulp.src(paths.scss.src).pipe(sass())
+        .pipe(gulp.dest(paths.scss.dest))
+        .pipe(concatCSS("styles.css"))
+        .pipe(gulp.dest(paths.scss.finalDest))
+        .pipe(rename({suffix: '.min'}))
+        .pipe(minifyCSS())
+        .pipe(gulp.dest(paths.scss.finalDest));
+    }));
+
+});
+
+/**
+ * Stylesheets one time task
+ */
+gulp.task('styles-one-time', function () {
+  return gulp.src(paths.scss.src).pipe(sass())
+    .pipe(gulp.dest(paths.scss.dest))
+    .pipe(rename({suffix: '.min'}))
+    .pipe(minifyCSS())
+    .pipe(gulp.dest(paths.scss.dest));
+});
+
+/**
+ * Javascript tasks
+ */
+gulp.task('js', function () {
+  gulp.src(paths.watch.js)
+    .pipe(watch(function (files) {
+      gulp.src(paths.scripts.src)
+        .pipe(concat(paths.scripts.dest))
+        .pipe(gulp.dest(paths.scripts.dir));
+
+      gulp.src(paths.scripts.fullDir)
+        .pipe(uglify({outSourceMap: false}))
+        .pipe(concat("scripts.min.js"))
+        .pipe(gulp.dest(paths.scripts.dir));
+    }));
+});
+
+
+/**
+ * Javascript one time tasks
+ */
+gulp.task('js-one-time', function () {
+  gulp.src(paths.scripts.src)
+    .pipe(concat(paths.scripts.dest))
+    .pipe(gulp.dest(paths.scripts.dir));
+
+  gulp.src(paths.scripts.fullDir)
+    .pipe(uglify({outSourceMap: false}))
+    .pipe(concat("scripts.min.js"))
+    .pipe(gulp.dest(paths.scripts.dir));
+});
+
+/**
+ * Imagemin task
+ */
+gulp.task('images', function () {
+  return gulp.src(paths.images.src)
+    // Pass in options to the task
+    .pipe(imagemin({optimizationLevel: 5}))
+    .pipe(gulp.dest(paths.images.dest));
+});
+
+/**
+ * JSHint task
+ */
+gulp.task('lint', function () {
+  gulp.src(paths.scripts.jsHint)
+    .pipe(jshint())
+    .pipe(jshint.reporter('default'));
+});
+
+/**
+ * Bower task
+ */
+gulp.task('bowerFiles', function () {
+  bowerFiles().pipe(gulp.dest(paths.bowerCopy.dest))
+});
+
+gulp.task('default', ['styles', 'js']);
+
+gulp.task('prod', ['styles-one-time', 'js-one-time', 'images', 'lint']);
